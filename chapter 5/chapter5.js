d7a17b548990d5c0d61faba60bb1ae1e0d8375ed
Fareed Sodha <fareedbsonly@gmail.com>
2024-03-01 23:19:13 +0500

    Chapter 5 Added

---
+++ chapter 5/chapter5.js
@@ -0,0 +1,31 @@
+var num1 = 2
+var num2 = 4
+
+var addition = num1 + num2
+var subtraction = num1 - num2
+var multiply = num1 * num2
+var divide = num1 / num2
+var modulas = num1 % num2
+var modulas = num1 ** num2
+
+console.log (addition)
+console.log (subtraction)
+console.log (multiply)
+console.log (divide)
+console.log (modulas)
+
+
+
+//percentage
+var math = 80
+var urdu = 78
+var english = 84
+var chemistry = 42
+var physics = 45
+
+var total = 450
+
+var add = math + urdu + english + chemistry + physics
+var percentage = add / total * 100
+
+console.log (percentage)
