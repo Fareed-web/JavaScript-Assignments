bd62429012c8c0ff89ecba8b7dc74c6b99313830
Fareed Sodha <fareedbsonly@gmail.com>
2024-03-16 06:02:20 +0500

    chapter 7 added

---
+++ chapter 7/chapter7.js
@@ -0,0 +1,47 @@
+// question 1
+
+let studentName = ['owais', 12]
+
+
+
+// question 2
+
+
+
+let studentName1 = {
+    name: "owais",
+    age: 12
+}
+
+
+// question 3
+let question3 = ["hi"]
+
+
+// question 4
+let question4 = [12]
+
+// question 5
+let question5 = [true]
+
+
+
+// question 6
+let question6 = ['h1',12,true]
+
+// q7
+let body = document.body
+let qul = ['SSC', 'HSC', 'BSC', 'BS', 'BCOM', 'MS', 'M.PHIL']
+
+document.write(qul)
+
+
+// q8
+
+let students = ['student1', 'student2', 'student3']
+let score = [500, 450 , 320]
+
+for (let i = 0; i < 3; i++) {
+    document.write(`Score of ${students[i]} is ${score[i]}. Percentage: ${(score[i] / 500) * 100}%`)
+}
+
