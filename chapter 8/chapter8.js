94a52863229b8a1dcbd1232e2bb0507ba091dee2
Fareed Sodha <fareedbsonly@gmail.com>
2024-03-21 00:51:49 +0500

    assignment 8 added

---
+++ chapter 8/chapter8.js
@@ -0,0 +1,39 @@
+// question 1
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+
+
+// question2
+for (let i = 0; i <= 20; i+=2){
+    console.log(i);
+}
+
+// question3
+for (let i = 1; i <= 15; i+=2){
+    console.log(i);
+}
+
+// question4
+var query = "query in q4"
+console.log(query)
+
+// question5
+for (let i = 1; i <= 10; i++  ){
+    console.log(7 + "X" + i + "=" + i * 7)
+}
+
+// question6 
+let arr = [1, 2, 3]
+let num = 0
+for (let i = 0; i < arr.length; i++){
+    num = num + arr[i]
+    console.log(num)
+}
+
+
+// question7
+let str = "olleh"
+for (let i = str.length; i >= 0; i--) {
+    console.log(str[i]);
+}
