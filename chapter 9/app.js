3e4ab1bc06e570c711e8d17f1f89c5f3b2e35cfa
Fareed Sodha <fareedbsonly@gmail.com>
2024-05-15 22:44:30 +0500

    new assignment added

--- chapter 9/app.js
+++ chapter 9/app.js
@@ -10,3 +10,4 @@ img1.addEventListener("click", () => {
 img2.addEventListener("click", () => {
     container.innerHTML = '<img id="img-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqTK_N9QV2PY4r9IOtkhkwKN1h_gNHxjuu5g&s" alt="">'
 })
+
